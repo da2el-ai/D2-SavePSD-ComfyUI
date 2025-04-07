fcc7cffe96c59ec354aad1f0b5b23f1f424230b8
da2el-ai <hirosyg.picasa@gmail.com>
2025-04-07 14:41:36 +0900

    feat: Add D2_SavePSD / D2_ApplyAlphaChannel / D2_ExtractAlpha

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""
+@author: da2el
+@title: D2 Save PSD
+@description: 
+"""
+from .nodes import NODE_CLASS_MAPPINGS
+
+WEB_DIRECTORY = "./web"
+NODE_DISPLAY_NAME_MAPPINGS = []
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
